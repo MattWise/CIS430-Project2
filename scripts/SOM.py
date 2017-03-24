59693b3fc3017c72e4230e10f1a430ad69d3f836
Matthew Wise <mwise@umassd.edu>
2017-03-24 14:24:46 -0400

    Added to SOM generation code.

--- scripts/SOM.py
+++ scripts/SOM.py
@@ -1,9 +1,40 @@
-from __future__ import division
+from __future__ import division,print_function
 
 import Orange
 
-DATA_FILE=r'../data/dataOccupancyPreprocessed.csv'
+import sil
 
-som=Orange.projection.som.SOMLearner(map_shape=(8, 8), initialize=Orange.projection.som.InitializeRandom)
+DATA_FILE=r'../data/dataOccupancyPreprocessed.csv'
+MAP_SHAPE=(8,8)
+
 data=Orange.data.Table(DATA_FILE)
-map = som(data)
+
+def iterate_over_map_shape(map_shape=MAP_SHAPE):
+    if len(map_shape)==1:
+        for i in range(map_shape[0]):
+            yield i
+    elif len(map_shape)==2:
+        for i in range(map_shape[0]):
+            for j in range(map_shape[1]):
+                yield i,j
+
+def make_net(map_shape=MAP_SHAPE, topology=0, neighbourhood=0, learning_rate=0.05, radius_ini=3, radius_fin=1, epochs=1000):
+    # http://orange.readthedocs.io/en/latest/reference/rst/Orange.projection.som.html#Orange.projection.som.Solver
+
+    som = Orange.projection.som.SOMLearner(map_shape=map_shape,
+                                           topology=topology,
+                                           neighbourhood=neighbourhood,
+                                           learning_rate=learning_rate,
+                                           radius_ini=radius_ini,
+                                           radius_fin=radius_fin,
+                                           epochs=epochs,
+                                           initialize=Orange.projection.som.InitializeRandom)
+
+    mp = som(data,progress_callback=print)
+    print("Finished")
+
+if __name__ == '__main__':
+    # make_net(epochs=20)
+    # print(data.domain)
+    for i in data[:3]:
+        print(i)
--- scripts/sil.py
+++ scripts/sil.py
@@ -1,56 +1,53 @@
 import numpy as np
 from numpy import linalg as LA
 
+
 #	Computes Sil Takes in 3 parameters
 #	a numpy array of dimensions number_of_items x number_of_features
 #	a numpy array which contains which cluster the nth item belongs too
 #	a function which computes the distance between 2 items
 
-def euclideanDistance(itm1 , itm2):
-	return LA.norm( itm1- itm2 )
-
-def computeSil( items , clusterNumber , distanceFunction = euclideanDistance):
-	
-	#Finds the max number of clusters and the needed space
-	maxClusterNum = np.amax(clusterNumber) + 1
-	tmpDistance = np.zeros(maxClusterNum)
-	clusterCount = np.zeros(maxClusterNum)
-	sil = np.empty(clusterNumber.size)
-	
-	i = 0
-	
-	for itm in items:
-		post = clusterNumber[i]
-		j = 0
-		for jtm in items:
-			if i != j:
-			
-				pos = clusterNumber[j]
-				
-				tmpDistance[pos] += distanceFunction(itm , jtm)
-				clusterCount[pos] += 1
-			j += 1
-			
-		a = tmpDistance[ post ] / clusterCount[post]
-		
-		#find the clusters closest to the one the item is currently in
-		
-		b = 1000
-		for k in range (0 , maxClusterNum):
-			tmp = tmpDistance[k] / clusterNumber[k]
-			if tmp < b and k != post and tmpDistance[k] != 0:
-				b = tmp
-		#compute silhouette coefficient
-		
-		if a == b:
-			sil[i] = 0.0
-		else:
-			sil[i] = (b - a) / max(a,b) 
-		
-		clusterCount.fill(0.0)
-		tmpDistance.fill(0.0)
-		i += 1
-	return sil
-	
+def euclideanDistance(itm1, itm2):
+    return LA.norm(itm1 - itm2)
 
 
+def computeSil(items, clusterNumber, distanceFunction=euclideanDistance):
+    # Finds the max number of clusters and the needed space
+    maxClusterNum = np.amax(clusterNumber) + 1
+    tmpDistance = np.zeros(maxClusterNum)
+    clusterCount = np.zeros(maxClusterNum)
+    sil = np.empty(clusterNumber.size)
+
+    i = 0
+
+    for itm in items:
+        post = clusterNumber[i]
+        j = 0
+        for jtm in items:
+            if i != j:
+                pos = clusterNumber[j]
+
+                tmpDistance[pos] += distanceFunction(itm, jtm)
+                clusterCount[pos] += 1
+            j += 1
+
+        a = tmpDistance[post] / clusterCount[post]
+
+        # find the clusters closest to the one the item is currently in
+
+        b = 1000
+        for k in range(0, maxClusterNum):
+            tmp = tmpDistance[k] / clusterNumber[k]
+            if tmp < b and k != post and tmpDistance[k] != 0:
+                b = tmp
+        # compute silhouette coefficient
+
+        if a == b:
+            sil[i] = 0.0
+        else:
+            sil[i] = (b - a) / max(a, b)
+
+        clusterCount.fill(0.0)
+        tmpDistance.fill(0.0)
+        i += 1
+    return sil
